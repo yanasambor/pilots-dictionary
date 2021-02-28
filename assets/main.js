c2e3b1bf64ded9f2053be6e75820ce56a4fdd568
yanasambor <yanasambor@gmail.com>
2021-02-28 09:40:02 +0300

    second

--- assets/main.js
+++ assets/main.js
@@ -1,55 +1,52 @@
-let abbreviation=abbreviationInput.value;
-
-let arr = abbreviation.split('');
-
-console.log(abbreviation);
-
-console.log(arr);
-
-let dictionary = [
-    {sign:"0",Trans:"Zero"},
-    {sign:"1",Trans:"One"},
-    {sign:"2",Trans:"Two"},
-    {sign:"3",Trans:"Three"},
-    {sign:"4",Trans:"Four"},
-    {sign:"5",Trans:"Five"},
-    {sign:"6",Trans:"Six"},
-    {sign:"7",Trans:"Seven"},
-    {sign:"8",Trans:"Eight"},
-    {sign:"9",Trans:"Nine"},
-    {sign:"A",Trans:"Alpha"},
-    {sign:"B",Trans:"Bravo"},
-    {sign:"C",Trans:"Charlie"},
-    {sign:"D",Trans:"Delta"},
-    {sign:"E",Trans:"Echo"},
-    {sign:"F",Trans:"Foxtrot"},
-    {sign:"G",Trans:"Golf"},
-    {sign:"H",Trans:"Hotel"},
-    {sign:"I",Trans:"India"},
-    {sign:"J",Trans:"Juliett"},
-    {sign:"K",Trans:"Kilo"},
-    {sign:"L",Trans:"Lima"},
-    {sign:"M",Trans:"Mike"},
-    {sign:"N",Trans:"November"},
-    {sign:"O",Trans:"Oscar"},
-    {sign:"P",Trans:"Papa"},
-    {sign:"Q",Trans:"Quebec"},
-    {sign:"R",Trans:"Romeo"},
-    {sign:"S",Trans:"Sierra"},
-    {sign:"T",Trans:"Tango"},
-    {sign:"U",Trans:"Uniform"},
-    {sign:"V",Trans:"Victor"},
-    {sign:"W",Trans:"Whiskey"},
-    {sign:"X",Trans:"X-ray"},
-    {sign:"Y",Trans:"Yankee"},
-  ]
-  console.log(dictionary);
-
+function decipher(){
+  let dictionary =new Map();
+    dictionary.set("0","Zero");
+    dictionary.set("1","One");
+    dictionary.set("2","Two");
+    dictionary.set("3","Three");
+    dictionary.set("4","Four");
+    dictionary.set("5","Five");
+    dictionary.set("6","Six");
+    dictionary.set("7","Seven");
+    dictionary.set("8","Eight");
+    dictionary.set("9","Nine");
+    dictionary.set("A","Alpha");
+    dictionary.set("B","Bravo");
+    dictionary.set("C","Charlie");
+    dictionary.set("D","Delta");
+    dictionary.set("E","Echo");
+    dictionary.set("F","Foxtrot");
+    dictionary.set("G","Golf");
+    dictionary.set("H","Hotel");
+    dictionary.set("I","India");
+    dictionary.set("J","Juliett");
+    dictionary.set("K","Kilo");
+    dictionary.set("L","Lima");
+    dictionary.set("M","Mike");
+    dictionary.set("N","November");
+    dictionary.set("O","Oscar");
+    dictionary.set("P","Papa");
+    dictionary.set("Q","Quebec");
+    dictionary.set("R","Romeo");
+    dictionary.set("S","Sierra");
+    dictionary.set("T","Tango");
+    dictionary.set("U","Uniform");
+    dictionary.set("V","Victor");
+    dictionary.set("W","Whiskey");
+    dictionary.set("X","X-ray");
+    dictionary.set("Y","Yankee");
+    dictionary.set("Z", "Zulu");
   
-function decipher(){ 
-  
-  for(i in dictionary){
-    if(dictionary[i].sign==me.value) 
-    abbreviation.innerHTML=dictionary[i].Trans;
+
+  abbreviation=(document.getElementById("abbreviationInput").value).toUpperCase();
+  let signs = abbreviation.split('');
+
+  let result = '';
+
+  for(let i=0; i<=signs.length; i++){
+    if(signs[i]>="A" && signs[i]<="Z" || signs[i]>="0" && signs[i]<="9"){
+      result+=` ${dictionary.get(signs[i])}`;
+    }
   }
+  document.getElementById("result").innerHTML = result;
 }
